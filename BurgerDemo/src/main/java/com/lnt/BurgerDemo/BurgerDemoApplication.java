2710cb60e4228edbe2a9f8fc0f133e07b2305e95
AE205_PC1 <AE205_PC1@Infva06510>
2019-08-20 10:18:37 -0700

    first app

---
+++ BurgerDemo/src/main/java/com/lnt/BurgerDemo/BurgerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lnt.BurgerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerDemoApplication.class, args);
+	}
+
+}
---
+++ BurgerDemo/src/test/java/com/lnt/BurgerDemo/BurgerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lnt.BurgerDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BurgerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
